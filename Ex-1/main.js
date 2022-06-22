52a79e5386b91c00df09d583c8f2ce04060f357e
ErJump <fedelijunior@gmail.com>
2022-06-22 15:03:45 +0200

    Can't use reverse - split  - join

--- Ex-1/main.js
+++ Ex-1/main.js
@@ -1,6 +1,6 @@
 let userWord = prompt('Inserisci una parola');
 
-function palindrome (word){
+/* function palindrome (word){
     //converto la stringa in un array attraverso split 
     //e inverto gli elementi degli array tramite reverse
     const wordArray = word.split('').reverse();
@@ -13,6 +13,6 @@ function palindrome (word){
     } else {
         console.log(`${word} non è una parola palindroma`)
     }
-}
+} */
 
 palindrome(userWord); 
