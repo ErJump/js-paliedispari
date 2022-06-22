baa73e9f7c260da8cb52cb947e0875b91ef7a908
ErJump <fedelijunior@gmail.com>
2022-06-22 13:48:22 +0200

    Palindroma complete

--- Ex-1/main.js
+++ Ex-1/main.js
@@ -7,6 +7,12 @@ function palindrome (word){
     //converto l'array in stringa tramite la funzione join
     const reverseWord = wordArray.join(''); 
     console.log(reverseWord);
+    //metto le condizioni di verifica
+    if (word === reverseWord){
+        console.log(`${word} è una parola palindroma`)
+    } else {
+        console.log(`${word} non è una parola palindroma`)
+    }
 }
 
 palindrome(userWord); 
