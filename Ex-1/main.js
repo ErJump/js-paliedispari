b5df06b155509ded65d55edde64187b18fc957ca
ErJump <fedelijunior@gmail.com>
2022-06-22 13:39:49 +0200

    Adds split to convert string to array

---
+++ Ex-1/main.js
@@ -0,0 +1,9 @@
+let userWord = prompt('Inserisci una parola');
+
+function palindrome (word){
+    //converto la stringa in un array attraverso split
+    const wordArray = word.split('');
+    console.log (wordArray);
+}
+
+palindrome(userWord); 
