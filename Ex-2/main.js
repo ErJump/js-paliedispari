983522b580821272c417c1f316242bb4b1d41646
ErJump <fedelijunior@gmail.com>
2022-06-22 14:02:59 +0200

    Adds control for user number

--- Ex-2/main.js
+++ Ex-2/main.js
@@ -1,9 +1,19 @@
-let oddOrEvenCheck = false
+let oddOrEvenCheck = false;
 let oddOrEvenChoice;
 
 while (oddOrEvenCheck === false){
     oddOrEvenChoice = prompt('Scegli "pari" o "dispari"');
     if (oddOrEvenChoice === 'pari' || oddOrEvenChoice === 'dispari'){
         oddOrEvenCheck = true;
+    }
+}
+
+let userNumberCheck = false;
+let userNumber;
+
+while (userNumberCheck === false){
+    userNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
+    if (!isNaN(userNumber) && userNumber <= 5 && userNumber >= 1){
+        userNumberCheck = true;
     }
 }
