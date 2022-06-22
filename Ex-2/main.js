dd90a3321837105f1407f7d21d2d7572cb1f6119
ErJump <fedelijunior@gmail.com>
2022-06-22 14:05:28 +0200

    Adds function to get random number

--- Ex-2/main.js
+++ Ex-2/main.js
@@ -17,3 +17,9 @@ while (userNumberCheck === false){
         userNumberCheck = true;
     }
 }
+
+function getRandom (min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+console.log(getRandom(1,5));
