5bf3217ccd7107375b583957b7b9fb4e45389bba
ErJump <fedelijunior@gmail.com>
2022-06-22 15:25:00 +0200

    For e mostra le lettere di metà parola in console

--- Ex-1/main.js
+++ Ex-1/main.js
@@ -1,5 +1,7 @@
 let userWord = prompt('Inserisci una parola');
 
+console.log(userWord.length)
+//Too easy
 /* function palindrome (word){
     //converto la stringa in un array attraverso split 
     //e inverto gli elementi degli array tramite reverse
@@ -14,5 +16,15 @@ let userWord = prompt('Inserisci una parola');
         console.log(`${word} non è una parola palindroma`)
     }
 } */
+
+function palindrome(word){
+    const wordLength = word.length;
+    //ciclo for basato su metà della lunghezza della parola
+    for (let i = 0; i < wordLength / 2; i++){
+        //ottengo in console la singola lettera della mezza parola ogni ciclo
+        console.log(word[i]);
+    }
+    
+}
 
 palindrome(userWord); 
